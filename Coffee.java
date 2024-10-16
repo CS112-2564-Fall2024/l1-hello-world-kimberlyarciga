53926393392d4bebb9b35627569fc4dfffa1167a
kimberlyarciga <kimberlyarciga04@gmail.com>
2024-10-16 20:45:23 +0000

    :3

--- Coffee.java
+++ Coffee.java
@@ -2,34 +2,34 @@ public class Coffee {
     private String type;
     private int size;
 
-    
+    // Constructor
     public Coffee(String type, int size) {
         this.type = type;
         this.size = size;
     }
 
-    
+    // Getter
     public String getType() {
         return type;
     }
 
-    
+    // Setter
     public void setType(String type) {
         this.type = type;
     }
 
-    
+    // Getter
     public int getSize() {
         return size;
     }
 
-    
+    // Setter
     public void setSize(int size) {
         this.size = size;
     }
 
     
-    
+    // ToString
     public String toString() {
         return "Coffee {" +
                 "Type = " + type + 
@@ -38,7 +38,7 @@ public class Coffee {
     }
 
     
-    
+    // Equals
     public boolean equals(Object obj) {
         if (this == obj) return true; 
         if (obj == null || getClass() != obj.getClass()) return false; 
